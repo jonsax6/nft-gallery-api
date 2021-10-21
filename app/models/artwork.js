b1928aceba0e1bd06fcd16d6a1f0baffc05b6ca8
jonsax6 <jdc.saxjunky@gmail.com>
2021-10-21 14:28:43 -0400

    initial commit

---
+++ app/models/artwork.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+
+const artworkSchema = new mongoose.Schema({
+  Artist: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  releaseDate: {
+    type: String,
+    required: true
+  },
+  medium: {
+    type: String,
+    required: true
+  },
+  artistRoyalty: {
+    type: Number,
+    required: true
+  },
+  curatorRoyalty: {
+    type: Number,
+    required: true
+  },
+  provenance: {
+    type: String,
+    required: true
+  },
+  exhibitionHistory: {
+    type: String
+  },
+  publishingHistory: {
+    type: String
+  },
+  notes: {
+    type: String
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Artwork', artworkSchema)
