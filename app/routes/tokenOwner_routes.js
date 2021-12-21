b3489e91290bd08d921a4f6a383a98209bc7e020
jonsax6 <jdc.saxjunky@gmail.com>
2021-12-21 00:59:00 -0500

    added tokenOwner routes, schema

--- app/routes/tokenOwner_routes.js
+++ app/routes/tokenOwner_routes.js
@@ -45,7 +45,7 @@ router.get('/token-owners', (req, res, next) => {
 
 // SHOW
 // GET /examples/5a7db6c74d55bc51bdf39793
-router.get('/token-owners/:id', (req, res, next) => {
+router.get('/token-owners/:id', requireToken, (req, res, next) => {
   // req.params.id will be set based on the `:id` in the route
   TokenOwner.findById(req.params.id)
     .then(handle404)
@@ -74,10 +74,10 @@ router.post('/token-owners', (req, res, next) => {
 
 // UPDATE
 // PATCH /examples/5a7db6c74d55bc51bdf39793
-router.patch('/token-owners/:id', removeBlanks, (req, res, next) => {
+router.patch('/token-owners/:id', requireToken, removeBlanks, (req, res, next) => {
   // if the client attempts to change the `owner` property by including a new
   // owner, prevent that by deleting that key/value pair
-  // delete req.body.tokenOwner.owner
+  delete req.body.tokenOwner.owner
 
   TokenOwner.findById(req.params.id)
     .then(handle404)
