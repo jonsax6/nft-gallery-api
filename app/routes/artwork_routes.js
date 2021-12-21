d441ec82c7add31ae760331fa2fe86a2fe7c3f08
jonsax6 <jdc.saxjunky@gmail.com>
2021-12-21 01:02:44 -0500

    removed ownership requirement to PATCH in artwork_routes

--- app/routes/artwork_routes.js
+++ app/routes/artwork_routes.js
@@ -84,7 +84,7 @@ router.patch('/artwork/:id', requireToken, removeBlanks, (req, res, next) => {
     .then(artwork => {
       // pass the `req` object and the Mongoose record to `requireOwnership`
       // it will throw an error if the current user isn't the owner
-      requireOwnership(req, artwork)
+      // requireOwnership(req, artwork)
 
       // pass the result of Mongoose's `.update` to the next `.then`
       return artwork.updateOne(req.body.artwork)
